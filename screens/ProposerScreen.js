d131c8ab39fa7fd78c448414db747eedabd7ef43
Aurelien <a.sanctorum1301@gmail.com>
2024-08-09 09:53:45 +0200

    URLchange

--- screens/ProposerScreen.js
+++ screens/ProposerScreen.js
@@ -37,7 +37,7 @@ export default function ProposerScreen({ navigation }) {
 
     const handleSubmit = () => {
         // Gérer l'envoi des données
-        fetch('http://192.168.1.78:3000/proposition', {
+        fetch('http://192.168.1.98:3000/proposition', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ 
